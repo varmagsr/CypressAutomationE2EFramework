11edddcf052ab29cd3cdd8fc3266c9fabc3017b5
varmagsr <varmagsr@gmail.com>
2022-10-31 16:51:22 -0400

    updated with reporting utility

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'tj562t',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -10,12 +11,11 @@ module.exports = defineConfig({
     screenshotsFolder: 'cypress/screenshots',
     defaultCommandTimeout: 6000,
     pageLoadTimeout: 10000,
+    reporter: 'mochawesome',
     "env" :
     {
       "QAUrl" : "https://rahulshettyacademy.com/angularpractice/",
       "QAUrlsp" : "https://rahulshettyacademy.com/angularpractice/shop"
-
-
     }
 
 
