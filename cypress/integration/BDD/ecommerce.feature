cdac4dc9b9eda711cac81877e59381a436f490c0
varmagsr <varmagsr@gmail.com>
2022-11-06 09:46:24 -0500

    New test cases added to BDD suite

--- cypress/integration/BDD/ecommerce.feature
+++ cypress/integration/BDD/ecommerce.feature
@@ -1,8 +1,15 @@
 Feature: End to End Ecommerce Validation
 
-    Feature Description
+    App Smoke Test
 
     Scenario: Ecommerce Product delivery
     Given I Open Ecommerce Page
     When I add items to cart
     Then select the country submit and verify Thankyou msg
+
+
+    Scenario: Filling the form to shop
+    Given I Open Ecommerce Page
+    When I fill the form details 
+    Then Validate the form behaviour
+    And select the shop page
--- cypress/integration/BDD/ecommerce/eCommStepDef.js
+++ cypress/integration/BDD/ecommerce/eCommStepDef.js
@@ -5,11 +5,11 @@ const homePage = new HomePage()
 
 Given('I Open Ecommerce Page',function(){
     cy.visit(Cypress.env('QAUrl'))
-    homePage.getShopPage().click()
+   
 })
 When('I add items to cart', function(){
   
-
+    homePage.getShopPage().click()
     this.data.productName.forEach(function(element){
        cy.selectProduct(element)
     }) 
@@ -24,3 +24,26 @@ Then('select the country submit and verify Thankyou msg', function(){
         cy.contains('600000')
 
 })
+
+
+
+When('I fill the form details', function(){
+    homePage.getNameEditBox().type(this.data.name)
+    homePage.getEmailEditBox().type(this.data.email)
+    homePage.getPasswordEditBox().type(this.data.pwd)
+    homePage.getIcecreamCheckBox().check()
+    homePage.getGenderEditBox().select(this.data.gender)
+    homePage.getEmploymentStatus().check()
+
+}) 
+Then('Validate the form behaviour', function(){
+   
+    homePage.getNameReflaction().should('have.value',this.data.name) 
+    homePage.getNameEditBox().should('have.attr','minlength','2')
+
+}) 
+And('select the shop page', function(){
+
+    homePage.getShopPage().click()
+
+}) 
